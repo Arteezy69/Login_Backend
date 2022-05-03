fbd7e09fd9a0a75a13e220fed7fbffee0b47724f
Art <103721700+Arteezy69@users.noreply.github.com>
2022-05-03 17:08:28 +0800

    Add files via upload

---
+++ loginBackend/validate.php
@@ -0,0 +1,21 @@
+<?php  
+include("connection.php");
+
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+
+
+$sql = "SELECT * FROM `loginform` WHERE user='$username' AND pass='$password' ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo("Password is correct");
+  } else {
+    echo "Incorrect Password";
+    $conn->close();
+  }
+?>
+
